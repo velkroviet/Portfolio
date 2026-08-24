6da41d3de9a4248729a5f869fb250ffa8ee487c2
Diarzoslava.Zhelezomrakovna.Veldrahova <276848202+velkroviet@users.noreply.github.com>
2026-08-25 01:57:15 +0900

    Add Stellar motion data processing and plotting
    This code loads star data, defines measurement parameters, and plots the intensity against wavelength. It also calculates the redshift and speed based on the minimum intensity.

---
+++ Stellar motion.m
@@ -0,0 +1,28 @@
+//このコードは、データを読み込み、測定パラメータを定義します。
+
+load starData
+nObs = size(spectra,1)
+lambdaStart = 630.02
+lambdaDelta = 0.14
+lambdaEnd = lambdaStart + (nObs-1)*lambdaDelta
+lambda = (lambdaStart:lambdaDelta:lambdaEnd)
+
+s = spectra(:,2)
+
+plot(lambda,s,".-")
+xlabel("Wavelength")
+ylabel("Intensity")
+
+
+[sHa,idx] = min(s)
+lambdaHa = lambda(idx)
+
+
+hold on
+plot(lambdaHa,sHa,"rs",MarkerSize=8)
+hold off
+
+
+
+z = lambdaHa/656.28 - 1
+speed = z*299792.458
